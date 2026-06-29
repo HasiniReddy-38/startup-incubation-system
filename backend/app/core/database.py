c602588d3c520ff04dcc111ddc7a7e22378486a7
HasiniReddy-38 <229x1a3301@gprec.ac.in>
2026-06-29 14:09:18 +0530

    Added DATABASE_URL environment support

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,10 +1,15 @@
+import os
 from urllib.parse import quote_plus
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 password = quote_plus("Nanna_@8341043342")
 
-DATABASE_URL = f"mysql+pymysql://root:{password}@localhost/innovation_db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    f"mysql+pymysql://root:{password}@localhost/innovation_db"
+)
 
 engine = create_engine(DATABASE_URL)
 
